5637bd2412944f1ae790a948c4e43202be9d7e03
charlesoxendine <102759864+charlesoxendine@users.noreply.github.com>
2022-08-22 21:48:57 -0400

    Initial Commit

---
+++ SwiftyForms/SwiftyForms.h
@@ -0,0 +1,18 @@
+//
+//  SwiftyForms.h
+//  SwiftyForms
+//
+//  Created by Charles Oxendine on 8/22/22.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for SwiftyForms.
+FOUNDATION_EXPORT double SwiftyFormsVersionNumber;
+
+//! Project version string for SwiftyForms.
+FOUNDATION_EXPORT const unsigned char SwiftyFormsVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <SwiftyForms/PublicHeader.h>
+
+
